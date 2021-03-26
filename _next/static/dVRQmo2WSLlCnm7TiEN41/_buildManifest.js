11ff955a251fdb1a6448d06eb2a771d1bb00aecf
PedroMGomes <pedrogomesprog@gmail.com>
2021-03-26 16:04:30 +0000

    module exports

---
+++ _next/static/dVRQmo2WSLlCnm7TiEN41/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-224a02eb8bd737881972.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-accde1a4e77a20b012b1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
