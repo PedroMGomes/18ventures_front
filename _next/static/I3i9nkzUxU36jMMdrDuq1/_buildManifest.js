6128b72b0b63d40ab57c0ece11ec1b0594a1001b
PedroMGomes <pedrogomesprog@gmail.com>
2021-03-26 17:06:46 +0000

    Fixed path

---
+++ _next/static/I3i9nkzUxU36jMMdrDuq1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-2f998217c45e8ac3cfff.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-accde1a4e77a20b012b1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
